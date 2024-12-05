0a877adc533eaba2a7633b0459bd98e8f3f7d95a
Noctivagous <119089025+noctivagous@users.noreply.github.com>
2024-12-04 18:01:05 -0600

    Add files via upload

--- ai/nctaichat/js/nctaichatui.js
+++ ai/nctaichat/js/nctaichatui.js
@@ -28,7 +28,7 @@ $(function () {
     }
 
     // Example usage for the accordion
-    for (i = 0; i < 20; i++) {
+    for (i = 0; i < 2; i++) {
 
         addResponseToAccordion("Q:? \n R: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..");
     }
